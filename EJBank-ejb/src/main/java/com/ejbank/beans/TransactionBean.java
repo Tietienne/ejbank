114e7453b3bab31f324c2ba9ca0a7363c8dc6c37
mehdi <mehdi@MacBook-Pro-de-mehdi.local>
2023-01-04 21:27:10 +0100

    transacation is done in both the accounts dest and source

--- EJBank-ejb/src/main/java/com/ejbank/beans/TransactionBean.java
+++ EJBank-ejb/src/main/java/com/ejbank/beans/TransactionBean.java
@@ -23,16 +23,23 @@ public class TransactionBean implements TransactionBeanLocal {
     @Override
     public AnswerPreviewPayload getAnswerPreview(PreviewPayload preview) {
         var source = em.find(Account.class, Integer.parseInt(preview.getSource()));
+        if(Integer.parseInt(preview.getAmount()) <= 0) {
+            return new AnswerPreviewPayload(false, source.getBalance(), source.getBalance() - Float.parseFloat(preview.getAmount()), "The transaction is not valid", null);
+        }
 
         if (source.getBalance() - Float.parseFloat(preview.getAmount()) < - source.getAccountType().getOverdraft() ) {
-            return new AnswerPreviewPayload(false, source.getBalance(), source.getBalance() - Float.parseFloat(preview.getAmount()), "Solde du compte source insuffisant", null);
+            return new AnswerPreviewPayload(false, source.getBalance(), source.getBalance() - Float.parseFloat(preview.getAmount()), "The transaction is not valid", null);
         }
-        return new AnswerPreviewPayload(true, source.getBalance(), source.getBalance() - Float.parseFloat(preview.getAmount()), source.getTransactions().get(0).getAmount().toString(), null);
+        return new AnswerPreviewPayload(true, source.getBalance(), source.getBalance() - Float.parseFloat(preview.getAmount()), "The transaction is valid", null);
     }
 
     public AnswerApplyPayload apply(ApplyPayload preview) {
         var source = em.find(Account.class, Integer.parseInt(preview.getSource()));
         var dest = em.find(Account.class, Integer.parseInt(preview.getDestination()));
+
+        if(preview.getAmount() <= 0) {
+            new AnswerApplyPayload(false,"Transaction can't be done");
+        }
 
         Calendar calendar = Calendar.getInstance();
         Date now = calendar.getTime();
@@ -40,6 +47,11 @@ public class TransactionBean implements TransactionBeanLocal {
         em.persist(new Transaction(Integer.parseInt(preview.getSource()),
                 Integer.parseInt(preview.getDestination()),
                 preview.getAuthor(),preview.getAmount(),preview.getComment(),true,now));
+
+        source.setBalance(source.getBalance()-preview.getAmount());
+        dest.setBalance(source.getBalance()+preview.getAmount());
+        em.flush();
+
 
         return new AnswerApplyPayload(true,"Transaction added");
     }
