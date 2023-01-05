8d8b970c8f67d01f6208193a00664124b928a8bc
Etienne ALEXANDRE <tietienne77@gmail.com>
2023-01-05 16:54:22 +0100

    fix: wrong value displayed / updated

--- EJBank-ejb/src/main/java/com/ejbank/beans/TransactionBean.java
+++ EJBank-ejb/src/main/java/com/ejbank/beans/TransactionBean.java
@@ -1,17 +1,14 @@
 package com.ejbank.beans;
 
 import com.ejbank.entity.Account;
-import com.ejbank.entity.Customer;
 import com.ejbank.entity.Transaction;
-import com.ejbank.entity.User;
 import com.ejbank.payload.transactions.*;
-
-import javax.transaction.*;
 
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -24,32 +21,29 @@ public class TransactionBean implements TransactionBeanLocal {
     @Override
     public AnswerPreviewPayload getAnswerPreview(PreviewPayload preview) {
         var source = em.find(Account.class, Integer.parseInt(preview.getSource()));
+        var dest = em.find(Account.class, Integer.parseInt(preview.getDestination()));
         if (Integer.parseInt(preview.getAmount()) <= 0 || source.getBalance() - Float.parseFloat(preview.getAmount()) < - source.getAccountType().getOverdraft() ) {
             return new AnswerPreviewPayload(false, source.getBalance(), source.getBalance() - Float.parseFloat(preview.getAmount()), "The transaction is not valid", null);
         }
-        return new AnswerPreviewPayload(true, source.getBalance(), source.getBalance() - Float.parseFloat(preview.getAmount()), "The transaction is valid", null);
+        return new AnswerPreviewPayload(true, source.getBalance() - Float.parseFloat(preview.getAmount()), dest.getBalance() + Float.parseFloat(preview.getAmount()), "The transaction is valid", null);
     }
+
     @Transactional
-    public AnswerApplyPayload apply(ApplyPayload preview) throws SystemException {
+    public AnswerApplyPayload apply(ApplyPayload preview) {
         var source = em.find(Account.class, Integer.parseInt(preview.getSource()));
         var dest = em.find(Account.class, Integer.parseInt(preview.getDestination()));
 
         if (preview.getAmount() <= 0 || source.getBalance() - preview.getAmount() < - source.getAccountType().getOverdraft() ) {
             new AnswerApplyPayload(false,"Transaction failed");
         }
-
-
         Calendar calendar = Calendar.getInstance();
         Date now = calendar.getTime();
-
-        em.persist(new Transaction(Integer.parseInt(preview.getSource()),
-                    Integer.parseInt(preview.getDestination()),
-                    preview.getAuthor(),preview.getAmount(),preview.getComment(),true,now));
-
+        em.persist(new Transaction(Integer.parseInt(preview.getDestination()),
+                Integer.parseInt(preview.getSource()),
+                preview.getAuthor(),preview.getAmount(),preview.getComment(),true,now));
         source.setBalance(source.getBalance()-preview.getAmount());
-        dest.setBalance(source.getBalance()+preview.getAmount());
+        dest.setBalance(dest.getBalance()+preview.getAmount());
         em.flush();
-
         return new AnswerApplyPayload(true,"Transaction added");
     }
 
